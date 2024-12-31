400771149eac8377fe879971c1efcc0c08de6e59
Chaimaridene <ridene.chaima@esprit.tn>
2024-12-31 16:22:36 +0100

    build: add the backend part

---
+++ backend/routes/hero.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const { create, list, byId, deleteHero, updateHero } = require('../controllers/hero.controller');
+
+router.post('/ajout', create)
+
+router.get('/list', list)
+
+router.get('/byid/:id', byId)
+
+router.delete('/delete/:id', deleteHero)
+
+router.put('/update/:id', updateHero)
+
+module.exports = router;
