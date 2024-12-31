d1adc7393b9b3ec2de8268adb628b80f441a4072
Chaimaridene <ridene.chaima@esprit.tn>
2024-12-31 16:23:10 +0100

    build: add the frontend part

---
+++ frontend/src/app/update/update.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../services/data.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-update',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './update.component.html',
+  styleUrl: './update.component.css'
+})
+export class UpdateComponent {
+
+  id: any;
+  hero: any;
+
+  constructor(
+     private _act: ActivatedRoute,
+     private _data: DataService,
+     private _router: Router
+    ){}
+
+  ngOnInit(): void{
+    //n9ra id ml lien
+    this.id = this._act.snapshot.paramMap.get('id');
+
+    this._data.byId(this.id).subscribe({
+      next: (res)=>{
+        this.hero = res;
+      }, 
+      error: (err)=>{
+        console.log(err);
+      },
+    });
+  }
+  save(){
+    this._data.update(this.id, this.hero).subscribe({
+      next: (res)=>{
+        this._router.navigate(['/list'])
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
+  }
+}
